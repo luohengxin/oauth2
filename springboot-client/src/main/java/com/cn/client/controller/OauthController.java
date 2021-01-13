2cd5c9433378963d74850353d4b9a5ff0bad3a2e
罗恒心 <924666778@qq.com>
2021-01-13 20:12:14 +0800

    登录页面

---
+++ springboot-client/src/main/java/com/cn/client/controller/OauthController.java
@@ -0,0 +1,45 @@
+package com.cn.client.controller;
+
+
+import com.cn.client.modle.OauthRequestBody;
+import com.cn.client.modle.TokenModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class OauthController {
+
+
+    @Value("${oauth.url}")
+    private String oauthUrl;
+
+    @GetMapping("/login.html")
+    public String login() {
+        return "/login";
+    }
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @PostMapping("/login.do")
+    public String login(@RequestBody OauthRequestBody requestBody) {
+
+        requestBody.setClient_id("client");
+        requestBody.setClient_secret("123456");
+        TokenModel tokenModel = restTemplate.postForObject(oauthUrl, requestBody, TokenModel.class);
+        // 登录失败 者需要转到重新登录页面
+        return requestBody.getRedirect_uri() + "?access_token=" + tokenModel.getAccess_token();
+    }
+
+}
---
+++ springboot-client/src/main/java/com/cn/client/modle/OauthRequestBody.java
@@ -0,0 +1,86 @@
+package com.cn.client.modle;
+
+public class OauthRequestBody {
+
+    private String response_type;
+    private String client_id;
+    private String client_secret;
+    private String redirect_uri;
+    private String scope;
+    private String grant_type;
+    private String refresh_token;
+    private String username;
+    private String password;
+
+    public String getResponse_type() {
+        return response_type;
+    }
+
+    public void setResponse_type(String response_type) {
+        this.response_type = response_type;
+    }
+
+    public String getClient_id() {
+        return client_id;
+    }
+
+    public void setClient_id(String client_id) {
+        this.client_id = client_id;
+    }
+
+    public String getClient_secret() {
+        return client_secret;
+    }
+
+    public void setClient_secret(String client_secret) {
+        this.client_secret = client_secret;
+    }
+
+    public String getRedirect_uri() {
+        return redirect_uri;
+    }
+
+    public void setRedirect_uri(String redirect_uri) {
+        this.redirect_uri = redirect_uri;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+    public String getGrant_type() {
+        return grant_type;
+    }
+
+    public void setGrant_type(String grant_type) {
+        this.grant_type = grant_type;
+    }
+
+    public String getRefresh_token() {
+        return refresh_token;
+    }
+
+    public void setRefresh_token(String refresh_token) {
+        this.refresh_token = refresh_token;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
